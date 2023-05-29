a70dc6e5981ad087b7dfed35f334519534aade44
Ericlys Moreira <ericlysm@gmail.com>
2023-05-29 14:16:30 -0300

    chore: abstracting creation of entities

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,6 +1,7 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 
 interface QuestionProps {
   authorId: UniqueEntityID
@@ -8,9 +9,17 @@ interface QuestionProps {
   title: string
   content: string
   slug: Slug
-  createdAt: string
-  updatedAt: string
+  createdAt: Date
+  updatedAt: Date
 }
 
 export class Question extends Entity<QuestionProps>{
+  static create(props: Optional<QuestionProps, 'createdAt'>, id?: UniqueEntityID){
+    const question = new Question({
+      ...props,
+      createdAt: new Date()
+    }, id)
+
+    return question
+  }
 }
