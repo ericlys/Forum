b5e92b92f2abdec3cd353d79c5e174bea6933ee2
Ericlys Moreira <ericlysm@gmail.com>
2023-06-02 18:29:42 -0300

    chore: delete answer comment use case

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -6,7 +6,21 @@ export class InMemoryAnswerCommentsRepository
 {
   public items: AnswerComment[] = []
 
+  async findById(id: string): Promise<AnswerComment | null> {
+    const answer = this.items.find((item) => item.id.toString() === id)
+
+    return answer ?? null
+  }
+
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment)
+  }
+
+  async delete(answerComment: AnswerComment): Promise<void> {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id,
+    )
+
+    this.items.splice(itemIndex, 1)
   }
 }
