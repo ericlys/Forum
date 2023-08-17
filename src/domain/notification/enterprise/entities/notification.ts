7aa3757b7200091086126e33bc58e5777b5d79e2
Ericlys Moreira <ericlysm@gmail.com>
2023-08-17 07:47:55 -0300

    chore: read notification use case

--- src/domain/notification/enterprise/entities/notification.ts
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -2,7 +2,7 @@ import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 
-interface NotificationProps {
+export interface NotificationProps {
   recipientId: UniqueEntityID
   title: string
   content: string
@@ -29,6 +29,11 @@ export class Notification extends Entity<NotificationProps> {
 
   get createdAt() {
     return this.props.createdAt
+  }
+
+  // assessor -permite alterar o valor da propriedade sem dar acesso diretamente
+  read() {
+    this.props.readAt = new Date()
   }
 
   static create(
