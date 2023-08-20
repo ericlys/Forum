dc36bb769f0437b2c8875a7459345f815d08163d
Ericlys Moreira <ericlysm@gmail.com>
2023-08-19 22:09:07 -0300

    chore: start domain events test

---
+++ src/core/events/domain-events.spec.ts
@@ -0,0 +1,27 @@
+import { AggregateRoot } from '../entities/aggregate-root'
+import { UniqueEntityID } from '../entities/unique-entity-id'
+import { DomainEvent } from './domain-event'
+
+class CustomAggregateCreated implements DomainEvent {
+  public ocurredAt: Date
+  private aggregate: CustomAggregate //eslint-disable-line
+
+  constructor(aggregate: CustomAggregate) {
+    this.ocurredAt = new Date()
+    this.aggregate = aggregate
+  }
+
+  public getAggregateId(): UniqueEntityID {
+    return this.aggregate.id
+  }
+}
+
+class CustomAggregate extends AggregateRoot<null> {
+  static create() {
+    const aggregate = new CustomAggregate(null)
+
+    aggregate.addDomainEvent(new CustomAggregateCreated(aggregate))
+
+    return aggregate
+  }
+}
