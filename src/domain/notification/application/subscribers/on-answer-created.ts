adad00c9fe13f8955857f2400716ec96934b9707
Ericlys Moreira <ericlysm@gmail.com>
2023-08-20 23:15:59 -0300

    chore: sending notification on subscriber

--- src/domain/notification/application/subscribers/on-answer-created.ts
+++ src/domain/notification/application/subscribers/on-answer-created.ts
@@ -1,9 +1,14 @@
 import { DomainEvents } from '@/core/events/domain-events'
 import { EventHandler } from '@/core/events/event-handler'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { AnswerCreateEvent } from '@/domain/forum/enterprise/events/answer-created-event'
+import { SendNotificationUseCase } from '../use-cases/send-notification'
 
 export class OnAnswerCreated implements EventHandler {
-  constructor() {
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private sendNotification: SendNotificationUseCase,
+  ) {
     this.setupSubscriptions()
   }
 
@@ -15,6 +20,20 @@ export class OnAnswerCreated implements EventHandler {
   }
 
   private async sendNewAnswerNotification({ answer }: AnswerCreateEvent) {
-    console.log(answer) // bind is used to use this here
+    // console.log(answer) // bind is used to use this here
+
+    const question = await this.questionsRepository.findById(
+      answer.questionId.toString(),
+    )
+
+    if (question) {
+      await this.sendNotification.execute({
+        recipientId: question.authorId.toString(),
+        title: `Nova resposta em "${question.title
+          .substring(0, 40)
+          .concat('...')}"`,
+        content: answer.excerpt,
+      })
+    }
   }
 }
